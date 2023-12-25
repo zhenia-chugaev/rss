38db6d4dcaa1ab7dc2324ca78488b448b2cd76f5
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-12-25 14:25:26 +0300

    Configure bootstrap compilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
   mode: isProduction ? 'production' : 'development',
   entry: './src/index.js',
+  output: {
+    filename: 'script.js',
+    clean: true,
+  },
   devServer: {
     open: true,
     host: 'localhost',
@@ -13,12 +18,24 @@ const config = {
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/i,
         loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
