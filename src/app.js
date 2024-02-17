473c55f3cbe913b9407054bff9cb1d18e9e4a740
zhenia-chugaev <zhenia.chugaev@bk.ru>
2024-02-17 21:11:20 +0300

    Refactor the source code

--- src/app.js
+++ src/app.js
@@ -2,10 +2,8 @@ import { makeAutoObservable, reaction, when } from 'mobx';
 import i18next from 'i18next';
 import { string, setLocale } from 'yup';
 import { renderForm, renderFeeds, renderPosts } from './render';
+import { onSubscriptionFormSubmit, subscribeToFeedUpdates } from './controllers';
 import resources from './locales';
-import { loadFeedContents } from './http';
-import { subscribeToFeedUpdates } from './controllers';
-import { parseXmlString, getFeedDetails, getTranslationKeyFromError } from './utils';
 import { FormStatuses } from './constants';
 
 const app = () => {
@@ -49,49 +47,7 @@ const app = () => {
   const feedsContainer = document.querySelector('#feeds');
   const postsContainer = document.querySelector('#posts');
 
-  subscriptionForm.addEventListener('submit', (e) => {
-    const formData = new FormData(e.target);
-    const rssUrl = formData.get('rss-url');
-    urlSchema.validate(rssUrl)
-      .then((url) => {
-        state.subscriptionForm = {
-          status: FormStatuses.LOADING,
-          message: '',
-        };
-        return loadFeedContents(url);
-      })
-      .then(({ data }) => {
-        const xmlDoc = parseXmlString(data.contents);
-        const error = xmlDoc.querySelector('parsererror');
-
-        if (error) {
-          throw new Error('invalidRss');
-        }
-
-        const { title, description, items } = getFeedDetails(xmlDoc);
-        const newFeed = { title, description };
-
-        state.feeds = [newFeed, ...state.feeds];
-        state.posts = [...items, ...state.posts];
-
-        return data.status.url;
-      })
-      .then((feedUrl) => {
-        state.feedUrls.push(feedUrl);
-        state.subscriptionForm = {
-          status: FormStatuses.SUBMITTED,
-          message: 'subscriptionForm.feedback.rssLoaded',
-        };
-        e.target.reset();
-      })
-      .catch((err) => {
-        state.subscriptionForm = {
-          status: FormStatuses.FAILED,
-          message: getTranslationKeyFromError(err),
-        };
-      });
-    e.preventDefault();
-  });
+  subscriptionForm.addEventListener('submit', onSubscriptionFormSubmit(state, urlSchema));
 
   reaction(() => state.subscriptionForm, () => renderForm(state, subscriptionForm, i18n));
   reaction(() => state.feeds, () => renderFeeds(state, feedsContainer, i18n));
--- src/controllers.js
+++ src/controllers.js
@@ -1,8 +1,51 @@
-/* eslint import/prefer-default-export: warn */
-
 import noop from 'lodash/noop';
 import { loadFeedContents } from './http';
-import { parseXmlString, getFeedDetails } from './utils';
+import { parseXmlString, getFeedDetails, getTranslationKeyFromError } from './utils';
+import { FormStatuses } from './constants';
+
+const onSubscriptionFormSubmit = (state, urlSchema) => (e) => {
+  const formData = new FormData(e.target);
+  const rssUrl = formData.get('rss-url');
+  urlSchema.validate(rssUrl)
+    .then((url) => {
+      state.subscriptionForm = {
+        status: FormStatuses.LOADING,
+        message: '',
+      };
+      return loadFeedContents(url);
+    })
+    .then(({ data }) => {
+      const xmlDoc = parseXmlString(data.contents);
+      const error = xmlDoc.querySelector('parsererror');
+
+      if (error) {
+        throw new Error('invalidRss');
+      }
+
+      const { title, description, items } = getFeedDetails(xmlDoc);
+      const newFeed = { title, description };
+
+      state.feeds = [newFeed, ...state.feeds];
+      state.posts = [...items, ...state.posts];
+
+      return data.status.url;
+    })
+    .then((feedUrl) => {
+      state.feedUrls.push(feedUrl);
+      state.subscriptionForm = {
+        status: FormStatuses.SUBMITTED,
+        message: 'subscriptionForm.feedback.rssLoaded',
+      };
+      e.target.reset();
+    })
+    .catch((err) => {
+      state.subscriptionForm = {
+        status: FormStatuses.FAILED,
+        message: getTranslationKeyFromError(err),
+      };
+    });
+  e.preventDefault();
+};
 
 const subscribeToFeedUpdates = (state, interval = 5000) => setInterval(
   () => Promise.allSettled(state.feedUrls.map(loadFeedContents))
@@ -23,4 +66,4 @@ const subscribeToFeedUpdates = (state, interval = 5000) => setInterval(
   interval,
 );
 
-export { subscribeToFeedUpdates };
+export { onSubscriptionFormSubmit, subscribeToFeedUpdates };
